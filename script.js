f61a8340860b63c06c88a21939ad7fe0fd072116
shax911 <82878451+shax911@users.noreply.github.com>
2021-05-03 21:01:48 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+var Number = +prompt('введите время в 24 часовом формате')
+switch (Number) {
+    case 1:
+      alert('час ночи');
+      break;
+    case 2:
+        alert('2 часа ночи');
+      break;
+    case 3:
+        alert('3 часа ночи');
+      break;
+    case 4:
+        alert('4 часа ночи');
+      break;
+    case 5:
+        alert('5 утра');
+      break;
+    case 6:
+        alert('6 утра');
+      break;
+    case 7:
+        alert('7 утра');
+      break;
+    case 8:
+        alert('8 утра');
+      break;
+    case 9:
+        alert('9 утра');
+      break;
+    case 10:
+        alert('10 утра');
+      break;
+    case 11:
+        alert('11 утра');
+      break;
+    case 12:
+        alert('12 часов дня');
+      break;
+    case 13:
+        alert('1 часа дня');
+      break;
+    case 14:
+        alert('2 часа дня');
+      break;
+    case 15:
+        alert('3 часа дня');
+      break;
+    case 16:
+        alert('4 часа дня');
+      break;
+    case 17:
+        alert('5 часов вечера');
+      break;
+    case 18:
+        alert('6 часов вечера');
+      break;
+    case 19:
+        alert('7 часов вечера');
+      break;
+    case 20:
+        alert('8 часов вечера');
+      break;
+    case 21:
+        alert('9 часов вечера');
+      break;
+    case 22:
+        alert('10 часов ночи');
+      break;
+    case 23:
+        alert('11 часов ночи');
+      break;
+    case 24:
+        alert('ровно 12 ночи');
+      break;
+    default:
+        alert('введите время в 24 часовом формате');
+      break;
+  }
